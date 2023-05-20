1584525aa8c99af4a288e167a34ff6cfb6f9392e
sweetblasphemy <nik2006@abv.bg>
2023-05-20 11:10:40 +0300

    Add project files.

---
+++ school/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace school
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            School s = new School("admk", 17);
+            s.s("f", 17);
+            School s1 = new School("fheauf", 26);
+            s1.s("m", 26);
+
+        }
+    }
+}
---
+++ school/School.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace school
+{
+    internal class School
+    {
+
+        private string gender;
+        private int age;
+
+
+        public string G
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+        public School(string gender, int age)
+        {
+            this.gender = gender;
+            this.age = age;
+        }
+
+        public void s(string gender, int age)
+        {
+
+            if (gender == "m" && age >= 16)
+            {
+
+                Console.WriteLine("Mr.");
+
+            }
+            else if (gender == "m" && age <= 15)
+            {
+
+                Console.WriteLine("Master");
+
+            }
+
+            if (gender == "f" && age >= 16)
+            {
+
+                Console.WriteLine("Ms.");
+
+            }
+            else if (gender == "f" && age <= 15)
+            {
+
+                Console.WriteLine("Miss");
+
+            }
+
+        }
+
+    }
+}
